26b895a369487decc2ac1968dafe5fdb60711068
Pandya S. <pandyasakaaji@gmail.com>
2025-11-14 19:26:16 +0700

    feat(memoirs): replace blog with memoirs including pages, navigation, and styling

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,8 +3,8 @@ import { glob } from "astro/loaders";
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
-const blog = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+const memoirs = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.md', base: "./src/memoirs" }),
 	schema: z.object({
 		title: z.string(),
 		pubDate: z.date(),
@@ -17,4 +17,4 @@ const blog = defineCollection({
 	})
 });
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { memoirs };
